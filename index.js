1c951bb8103da1020db4ac5dd89f041e80fbca8f
unknown <Martin@.(none)>
2015-01-25 19:51:31 +0100

    Update

--- index.js
+++ index.js
@@ -2,17 +2,40 @@
 var http = require('http');
 express = require('express');
 
+
+var fs = require("fs"),
+path = require("path");
 //2
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type' : 'text/html'
-	});
-	res.end('<html><body><h1>Hello World</h1></body></html>');
-}).listen(3000);
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+/* 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+  });
+  
+// List files
+*/
 
-console.log('Server running on port 3000.');
+console.log("Listing");
+
+var p = "..\\..";
+fs.readdir(p, function (err, files) {
+    if (err) {
+    	console.log("Error");
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(p, file);
+ //   }).filter(function (file) {
+ //       return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+        console.log("%s (%s) %d", file, path.extname(file),fs.statSync(file).isFile());
+    });
+});
