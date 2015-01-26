7002c2003afb9642ad31edfdb257c2780f205bdf
Martin von der Mey <mvondermey@gmail.com>
2015-01-26 10:45:02 +0100

    Update

--- websync.js
+++ websync.js
@@ -1,29 +1,13 @@
 //1
 var http = require('http');
 express = require('express');
-
-
+//
 var fs = require("fs"),
 path = require("path");
-//2
-
+//
 var app = express();
 app.set('port', process.env.PORT || 3000);
-
-
-app.get('/', function (req, res) {
-  res.send('<html><body><h1>Hello World</h1></body></html>');
-});
-/* 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-  });
-  
-// List files
-*/
-
-console.log("Listing");
-
+//
 var p = "..\\..";
 fs.readdir(p, function (err, files) {
     if (err) {
